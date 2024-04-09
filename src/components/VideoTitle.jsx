0387116d71731af6811cec9b5b87aa5d3349cca5
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-04-09 21:56:18 +0530

    added video title

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { TMDB_MOVIE_POSTER } from "../utils/constants";
 import { useEffect, useState } from "react";
+import { Star, Check, Play, Info, Plus, ShoppingBagIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 const VideoTitle = ({ movieId, title, language, isAdult, rating, thumbnail, currentIndex }) => {
   const trailer = useSelector((store) => store.movies?.trailerVideo);
@@ -32,7 +34,57 @@ const VideoTitle = ({ movieId, title, language, isAdult, rating, thumbnail, curr
       style={{
         background: `linear-gradient(to right, black ${backgroundImage ? "20%" : "40%"}, transparent), url(${backgroundImage}) no-repeat center center/cover`,
       }}>
-      VideoTitle
+      <div>
+        <div className="flex">
+          <span className="flex text-xs mdLtext-lg pe-2">
+            <div className="bg-yellow-500 rounded-sm px-1 text-black font-extrabold me-2">TMDb</div>
+            {rating ? rating.toFixed(1) : ""}
+          </span>
+          <Star color="#f4d50b" fill="#f4d50b" strokeWidth={3} className="md:mt-[2px] -mt-1 w-2 md:w-4" />{" "}
+        </div>
+        <div className="max-w-fit">
+          <h1 className="text-6xl mt-5 py-2 rounded-lg mb-1 bg-gradient-to-r from-transparent via-sky-600 to-transparent">{movieLogo}</h1>
+          <p className="text-center text-xs md:text-lg">{languageNames.of(language)}</p>
+        </div>
+        <div className="mt-2 md:mt-12 lg:md-6 flex">
+          {isTrailerAvailable ? (
+            <span className="flex text-xs md:text-lg">
+              <Check strokeWidth={5} color="#000000" className="bg-sky-500 h-4 rounded-full p-1 me-2 mt-1 w-4 md:w-6 md:h-6" />
+              Included with Prime
+            </span>
+          ) : (
+            <span className="flex text-xs md:text-lg">
+              <ShoppingBagIcon strokeWidth={2} color="#fbbf24" className="h-4 me-2 w-4 md:w-6 md:h-6" />
+              Available to rent
+            </span>
+          )}
+          {isTrailerAvailable && <div className="ps-5 text-sm md:text-xl flex items-center">Play Trailer</div>}
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="bg-neutral-700 rounded-full p-3 w-fit h-fit ms-10 cursor-pointer hidden md:block">
+                  <Plus size={28} color="#ffffff" strokeWidth={3} />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-lg">Watchlist </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="bg-neutral-700 rounded-full p-2 md:p-3 w-fit h-fit ms-2 cursor-pointer">
+                  <Info color="#ffffff" strokeWidth={2} className="h-3 w-3 md:h-8 md:w-8" />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-xs md:text-lg">Details</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
     </div>
   );
 };
