80f651e52397a17b5dae3b5d57194e1fb6a26af8
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-04-25 01:15:37 +0530

    added tabs

--- src/Pages/MoviePlayer.jsx
+++ src/Pages/MoviePlayer.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import MovieDetails from "./MovieDetails";
 import Footer from "../common/Footer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import RecommendedMovies from "../components/RecommendedMovies";
+import MovieDtls from "../components/MovieDtls";
 
 const MoviePlayer = () => {
   return (
-    <div>
-      <MovieDetails />
-      <div className="mt-auto">
-        <Footer />
+    <div className="flex flex-col min-h-[150vh]">
+      <div className=" overflow-y-auto h-[85vh]">
+        <MovieDetails />
       </div>
+      <div className="-mt-5 text-center z-20 text-white">
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="related">Related</TabsTrigger>
+            <TabsTrigger value="details">Details</TabsTrigger>
+          </TabsList>
+          <TabsContent value="related">
+            <RecommendedMovies />
+          </TabsContent>
+          <TabsContent value="details">
+            <MovieDtls />
+          </TabsContent>
+        </Tabs>
+      </div>
+      <Footer />
     </div>
   );
 };
